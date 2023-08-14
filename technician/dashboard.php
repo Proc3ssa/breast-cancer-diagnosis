6e80b5c5e0a6f30b90923d4e327647d7273ba164
Processor <Pros3sa@gmail.com>
2023-08-14 06:19:12 +0000

    technician

---
+++ technician/dashboard.php
@@ -0,0 +1,119 @@
+<?php
+session_start();
+if(!isset($_SESSION['technician'])){
+    header("location:index.php");
+}
+include "../connection.php";
+$technician = $_SESSION['technician'];
+
+function convert($date):string{
+
+    $new_date = date("d F, Y", strtotime($date));
+
+   return $new_date;
+}
+
+$QUERY = mysqli_query($connection, "SELECT name from technicians WHERE email = '$technician'");
+
+$res = mysqli_fetch_assoc($QUERY);
+//patients fetch and search
+
+
+
+if(isset($_POST['search'])){
+    $patient = $_POST['patient'];
+    $primary_select = "SELECT firstname, email, lastname from patient where firstname LIKE '$patient%' or lastname LIKE '$patient%' or email LIKE '%$patient%'";
+
+    //echo $primary_select;
+}
+else{
+    $primary_select = "SELECT firstname, email, lastname FROM patient";
+}
+
+$primary_query = mysqli_query($connection, $primary_select);
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/ddashboard.css">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+    <title>technician dashboard</title>
+</head>
+<body>
+    <div class="sidebar">
+            <div class="patient">
+                <div class="image">
+                    <img src="../images/avatar.png" alt="pateint">
+                </div>
+                <h3><?php echo $res['name']?></h3>
+            </div>
+
+            <div class="menu">
+                <a href=""><div class="menu-item current">Patient  <div class="imag"><img src="../images/patient.svg" alt="" ></div> </div></a>
+
+               
+
+
+                
+            </div>
+
+    </div>
+
+    <!-- main -->
+
+    <div class="main">
+       <div class="date">
+        <b><?php echo convert(date("d-m-Y"))?></b>
+       </div>
+
+      
+       <div class="below">
+        <form action="#" method="POST">
+            
+            <div class="search">
+            <input type="search" name="patient" id="" placeholder="search for patients">
+            </div>
+
+            <div class="lense">
+            <button type="submit" name="search"><img src="../images/search.svg" alt=""></button>
+            </div>
+            
+        </form>
+
+        <div class="patientss">
+            <?php
+                while($pf = mysqli_fetch_assoc($primary_query)){
+                    echo '
+                            <a href="patient.php?patient='.$pf['email'].'">
+                    <div class="patient">
+                        <div>
+                            <img src="../images/patientb.svg" alt="" style="color: aliceblue;">
+                        </div>
+                        
+                        <b>'.$pf['firstname'].' '.$pf['lastname'].'</b>
+                    </div></a>
+                    ';
+
+                }
+            ?>
+            
+
+            
+
+
+           
+        </div>
+      
+
+       </div>
+
+    </div>
+    
+</body>
+</html>
---
+++ technician/index.php
@@ -0,0 +1,219 @@
+<?php
+if(isset($_POST['signin'])){
+  include "../connection.php";
+
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+
+  $SELECT = "SELECT *FROM technicians where email = '$email'and password = '$password'";
+
+  $query = mysqli_query($connection, $SELECT);
+
+  if($query -> num_rows == 0){
+    echo '
+    <script>
+    alert("Error:incorrect email or password");
+    </script>
+    ';
+  }
+
+  else{
+    session_start();
+    $_SESSION['technician'] = $email;
+    header("location:dashboard.php");
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+
+  <title>Cancer Diagnosis | Technician</title>
+
+
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="../css/bootstrap.css" />
+
+  <!-- fonts style -->
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
+
+  <!--owl slider stylesheet -->
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+
+  <!-- font awesome style -->
+  <link href="../css/font-awesome.min.css" rel="stylesheet" />
+  <!-- nice select -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/css/nice-select.min.css" integrity="sha256-mLBIhmBvigTFWPSCtvdu6a76T+3Xyt+K571hupeFLg4=" crossorigin="anonymous" />
+  <!-- datepicker -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker.css">
+  <!-- Custom styles for this template -->
+  <link href="../css/style.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="../css/responsive.css" rel="stylesheet" />
+  <script src="js/custom.js"></script>
+
+</head>
+
+<body class="sub_page">
+
+  
+
+
+  <!-- contact section -->
+  <section class="contact_section layout_padding-bottom" id="section1">
+    <div class="container">
+      
+      <div class="heading_container">
+        <h2>
+          Sign In <span class="myspan">-Technician</span>
+        </h2>
+      </div>
+      <div class="row">
+        <div class="col-md-7">
+          <div class="form_container">
+            <form action="#" method="POST">
+             
+              <div>
+                <input type="email" placeholder="Email" name="email" required />
+              </div>
+              <div>
+                <input type="password" placeholder="Password" name="password" required/>
+              </div>
+             
+              <div class="btn_box">
+                <button type="submit" name="signin">
+                  LOGIN
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  </section>
+  <!-- end contact section -->
+
+  <!-- info section -->
+  <section class="info_section ">
+    <div class="container">
+     
+      <div class="info_bottom layout_padding2">
+        <div class="row info_main_row">
+          <div class="col-md-6 col-lg-3">
+            <h5>
+              Address
+            </h5>
+            <div class="info_contact">
+              <a href="">
+                <i class="fa fa-map-marker" aria-hidden="true"></i>
+                <span>
+                  Ghana
+                </span>
+              </a>
+              <a href="">
+                <i class="fa fa-phone" aria-hidden="true"></i>
+                <span>
+                  Call +233 1234567890
+                </span>
+              </a>
+              <a href="">
+                <i class="fa fa-envelope"></i>
+                <span>
+                  me@gmail.com
+                </span>
+              </a>
+            </div>
+            <div class="social_box">
+              <a href="">
+                <i class="fa fa-facebook" aria-hidden="true"></i>
+              </a>
+              <a href="">
+                <i class="fa fa-twitter" aria-hidden="true"></i>
+              </a>
+              <a href="">
+                <i class="fa fa-linkedin" aria-hidden="true"></i>
+              </a>
+              <a href="">
+                <i class="fa fa-instagram" aria-hidden="true"></i>
+              </a>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <div class="info_links">
+              <h5>
+                Useful link
+              </h5>
+              <div class="info_links_menu">
+                <a href="index.html">
+                  Home
+                </a>
+                <a href="about.html">
+                  About
+                </a>
+                <a href="treatment.html">
+                  Treatment
+                </a>
+                <a href="doctor.html">
+                  Doctors
+                </a>
+                <a href="testimonial.html">
+                  Testimonial
+                </a>
+                <a class="active" href="contact.html">
+                  Contact us
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3">
+          
+          </div>
+          <div class="col-md-6 col-lg-3">
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- end info_section -->
+
+
+  <!-- footer section -->
+  <footer class="footer_section">
+    <div class="container">
+      <p>
+        &copy; <span id="displayYear"></span>
+      </p>
+    </div>
+  </footer>
+  <!-- footer section -->
+
+  <!-- jQery -->
+  <script src="js/jquery-3.4.1.min.js"></script>
+  <!-- bootstrap js -->
+  <script src="js/bootstrap.js"></script>
+  <!-- nice select -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/js/jquery.nice-select.min.js" integrity="sha256-Zr3vByTlMGQhvMfgkQ5BtWRSKBGa2QlspKYJnkjZTmo=" crossorigin="anonymous"></script>
+  <!-- owl slider -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+  <!-- datepicker -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/js/bootstrap-datepicker.js"></script>
+  <!-- custom js -->
+  <script src="js/custom.js"></script>
+
+
+</body>
+
+</html>
